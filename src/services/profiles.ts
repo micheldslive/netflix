ad7b7516c89e3adae63e1e57bf83ba55ca8a6fc5
Michel Domingos <micheldslive@gmail.com>
2021-10-22 22:21:11 -0300

    first commit

--- src/services/profiles.ts
+++ src/services/profiles.ts
@@ -1,8 +1,8 @@
-import Avatar1 from "assets/images/avatar1.png";
-import Avatar2 from "assets/images/avatar2.png";
-import Avatar3 from "assets/images/avatar3.png";
-import Avatar4 from "assets/images/avatar4.png";
-import Avatar5 from "assets/images/avatar5.png";
+import Avatar1 from "assets/images/avatar1.jpg";
+import Avatar2 from "assets/images/avatar2.jpg";
+import Avatar3 from "assets/images/avatar3.jpg";
+import Avatar4 from "assets/images/avatar4.jpg";
+import Avatar5 from "assets/images/avatar5.jpg";
 
 export const profiles: { id: number; name: string; avatar: string }[] = [
   {
